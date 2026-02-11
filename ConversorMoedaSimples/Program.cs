4cc8d26112825ffe41593fe97c15e476986a22e3
José de Senna <josespnt1990@gmail.com>
2026-02-11 16:14:01 -0300

    feat: cria logica de uso para a classe ConversorDeMoeda

--- ConversorMoedaSimples/Program.cs
+++ ConversorMoedaSimples/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace ConversorMoedaSimples;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Digite a cotação do dólar: R$ ");
+        double cotacao = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+        Console.WriteLine("Digite o valor em dólar: U$ ");
+        double dolar = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
+
+        double valorFinal = ConversorDeMoeda.CalcularValorFinal(cotacao, dolar);
+
+        Console.WriteLine($"Valor a ser pago em reais: R$ {valorFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
